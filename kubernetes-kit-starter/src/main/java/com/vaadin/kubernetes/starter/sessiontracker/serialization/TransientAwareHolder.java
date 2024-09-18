b7793063936503f6e06992749be364be920f76ce
Johannes Tuikkala <johannes@vaadin.com>
2024-09-18 12:24:39 +0300

    #140 fixes for the deserialization of scoped beans
    Fixes from @mcollovati applied: inject transient objects with thread local UI and VaadinSession

--- kubernetes-kit-starter/src/main/java/com/vaadin/kubernetes/starter/sessiontracker/serialization/TransientAwareHolder.java
+++ kubernetes-kit-starter/src/main/java/com/vaadin/kubernetes/starter/sessiontracker/serialization/TransientAwareHolder.java
@@ -9,10 +9,19 @@
  */
 package com.vaadin.kubernetes.starter.sessiontracker.serialization;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.internal.CurrentInstance;
+import com.vaadin.flow.internal.ReflectTools;
+import com.vaadin.flow.server.VaadinSession;
+
 import java.io.Serializable;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * A serializable class that holds information about an object to be
@@ -28,10 +37,14 @@ final class TransientAwareHolder implements Serializable {
 
     private final List<TransientDescriptor> transientDescriptors;
     private final Object source; // NOSONAR
+    private final VaadinSession session;
+    private final UI ui;
 
     TransientAwareHolder(Object source, List<TransientDescriptor> descriptors) {
         this.source = source;
         this.transientDescriptors = new ArrayList<>(descriptors);
+        this.session = VaadinSession.getCurrent();
+        this.ui = UI.getCurrent();
     }
 
     /**
@@ -51,6 +64,50 @@ final class TransientAwareHolder implements Serializable {
      */
     Object source() {
         return source;
+    }
+
+    void runWithVaadin(Runnable runnable) {
+        Map<Class<?>, CurrentInstance> old = null;
+        if (ui != null) {
+            old = CurrentInstance.setCurrent(ui);
+        } else if (session != null) {
+            old = CurrentInstance.setCurrent(session);
+        }
+        try {
+            executeWithSessionLock(session, runnable);
+        } finally {
+            if (old != null) {
+                CurrentInstance.restoreInstances(old);
+            }
+        }
+    }
+
+    private static void executeWithSessionLock(VaadinSession session,
+                                               Runnable runnable) {
+        if (session != null) {
+            Lock lock = session.getLockInstance();
+            Field lockField = null;
+            if (lock == null) {
+                lock = new ReentrantLock();
+                try {
+                    lockField = VaadinSession.class.getDeclaredField("lock");
+                    ReflectTools.setJavaFieldValue(session, lockField, lock);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+            lock.lock();
+            try {
+                runnable.run();
+            } finally {
+                lock.unlock();
+                if (lockField != null) {
+                    ReflectTools.setJavaFieldValue(session, lockField, null);
+                }
+            }
+        } else {
+            runnable.run();
+        }
     }
 
 }
